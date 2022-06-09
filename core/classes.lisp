b25ce172b41ca48b67c6bcdb924ce2d2c5612c31
Tomáš Zellerin <tomas@zellerin.cz>
2022-06-09 14:48:19 +0200

    Documentation update.

--- core/classes.lisp
+++ core/classes.lisp
@@ -89,26 +89,7 @@
                      :seen-text-header nil
    )
   (:documentation
-   "An independent, bidirectional sequence of frames exchanged between
-  the client and server within an HTTP/2 connection.  Streams have
-  several important characteristics:
-
-  A single HTTP/2 connection can contain multiple concurrently open
-  streams, with either endpoint interleaving frames from multiple
-  streams.
-
-  Streams can be established and used unilaterally or shared by
-  either the client or server.
-
-  Streams can be closed by either endpoint.
-
-  The order in which frames are sent on a stream is significant.
-  Recipients process frames in the order they are received.  In
-  particular, the order of HEADERS and DATA frames is semantically
-  significant.
-
-  Streams are identified by an integer.  Stream identifiers are
-  assigned to streams by the endpoint initiating the stream."))
+   "Representation of HTTP/2 stream. See RFC7540."))
 
 (defclass client-stream (http2-stream)
   ((status :accessor get-status :initarg :status
