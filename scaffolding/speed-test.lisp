0709f9f1c047096adc8639e1a264c060c5c14b91
Tomáš Zellerin <tomas@zellerin.cz>
2023-04-28 10:30:58 +0200

    (doc, test) Add speed test script. README update about speed.
    h2load was chosen as it can compare http/1.1 and http2. In addition, it can do
    h2c over plain sockets.

---
+++ scaffolding/speed-test.lisp
@@ -0,0 +1,65 @@
+
+
+(load "~/quicklisp/setup")
+(asdf::load-asd (truename "./http2.asd"))
+(ql:quickload "http2/server/example" :silent t)
+(ql:quickload "hunchentoot" :silent t)
+(ql:quickload 'cl-ppcre :silent t)
+(ql:quickload 'woo :silent t)
+
+
+(in-package :http2/server-example)
+
+(declaim (optimize speed (safety 1) (debug 1)))
+
+(bt:make-thread (lambda () (ignore-errors
+                            (http2/server-example::run-demo-server :port 1237)))
+                :name "Our server thread")
+
+(bt:make-thread (lambda () (ignore-errors
+                            (http2::create-http-server  1238)))
+                :name "Our server thread")
+
+(bt:make-thread
+ (lambda ()
+   (woo:run
+    (lambda (env)
+      (declare (ignore env))
+      '(200 (:content-type "text/plain") ("Hello, World")))
+    :port 1239)))
+
+(hunchentoot:start (make-instance 'hunchentoot:easy-ssl-acceptor
+                                  :port 1235
+                                  :ssl-privatekey-file "/tmp/server.key"
+                                  :ssl-certificate-file "/tmp/server.crt"
+                                  :access-log-destination nil))
+
+(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 1236
+                                                             :access-log-destination nil))
+(flet ((scan-port (base name &rest args )
+         (let ((res (uiop:run-program `("h2load" "-n" "5000"
+                                                 ,@args)
+                                      :output :string)))
+           (multiple-value-bind (match val)
+               (cl-ppcre:scan-to-strings "req/s[ :]*([0-9\\.]+)[ ]*([0-9\\.]+)[ ]*([0-9\\.]+)" res)
+             (let ((val-as-num (read-from-string (aref val 2))))
+               (format t "~40a ~10@a~@[~5@a%~]~%" name val-as-num
+                       (when base (floor (/ val-as-num base 0.01))))
+               val-as-num)))))
+
+  (let ((base (scan-port nil "Hunchetoot over https" "https://localhost:1235")))
+    (scan-port base "HTTP/2 over TLS" "https://localhost:1237")
+    (scan-port base "HTTP/2 over plain socket" "http://localhost:1238")
+    (scan-port base "Hunchentoot on plain socket" "http://localhost:1236" "-p" "http/1.1")
+    (scan-port base "Woo on plain socket" "http://localhost:1239" "-p" "http/1.1")
+    (scan-port base "HTTP/2 over plain socket, -m20" "http://localhost:1238"
+               "-m" "20")
+    (scan-port base "Hunchentoot on plain socket, -m20" "http://localhost:1236"
+               "-p" "http/1.1" "-m" "20" )
+
+    (scan-port base "Woo on plain socket, -m20" "http://localhost:1239" "-p" "http/1.1"
+               "-m" "20" "-D1") ;this hangs when count-based for obvious reasons
+    (scan-port base "HTTP/2, plain socket, post " "http://localhost:1238"
+               "-d" "./client/client.lisp")
+    (scan-port base "Hunchentoot on plain socket, post" "http://localhost:1236" "-p" "http/1.1"
+               "-d" "./client/client.lisp")))
