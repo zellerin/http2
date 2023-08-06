722726cb57dd3147fb389aef9f82b04bd8b6a5db
Tomáš Zellerin <tomas@zellerin.cz>
2023-08-06 20:57:20 +0200

    (doc) Add documentation for pipe.lisp

--- tests/pipe.lisp
+++ tests/pipe.lisp
@@ -1,12 +1,38 @@
 ;;;; Copyright 2022 by Tomáš Zellerin
 
 (in-package :http2)
-;;;; Implement pipes for testing client-server pairs.
-;;;; No synchronization, usage in concurrently running processes not expected.
+
+(mgl-pax:defsection  @buffer-stream-and-pipes
+  (:title "Vector backed streams and (buffered) octet pipes")
+
+"In tests we need to send data between client and server as if using a
+stream. For this, we implement pipes that allow to send data to one stream and receive them from another (MAKE-PIPE), and possibly also vice versa (MAKE-FULL-PIPE).
+
+The pipe streams are backed by vectors.
+
+This should probably be some existing library, but I failed to find it.
+
+No synchronization, usage in concurrently running processes not expected."
+  (make-pipe function)
+  (make-full-pipe function)
+  (pipe-end-for-read class)
+  (pipe-end-for-write class)
+  (@buffer-stream-and-pipes-impl mgl-pax:section))
+
+(mgl-pax:defsection @buffer-stream-and-pipes-impl
+    (:title "Gray stream classes for vector backed stream"
+     :export nil)
+  (trivial-gray-streams:stream-read-byte (mgl-pax:method () (pipe-end-for-read)))
+  (trivial-gray-streams:stream-listen (mgl-pax:method () (pipe-end-for-read)))
+  (trivial-gray-streams:stream-write-byte (mgl-pax:method () (pipe-end-for-write))))
 
 (defclass pipe-end-for-read (binary-stream trivial-gray-streams:fundamental-binary-input-stream)
   ((buffer :accessor get-buffer :initarg :buffer)
-   (index  :accessor get-index  :initarg :index)))
+   (index  :accessor get-index  :initarg :index))
+  (:documentation "Octet stream backed by a buffer.
+
+Each instance has a buffer and an index to the first unread element of it. It is
+assumed that whole the buffer can be read."))
 
 (defclass pipe-end-for-write (binary-stream trivial-gray-streams:fundamental-binary-output-stream)
   ((buffer :accessor get-buffer :initarg :buffer)))
@@ -41,16 +67,14 @@ read from the other."
       (prog1 (aref (get-buffer stream) (get-index stream))
         (incf (get-index stream)))))
 
-(defmethod trivial-gray-streams:stream-listen ((stream pipe-end-for-read))
-  (< (get-index stream) (length (get-buffer stream))))
-
 (defmethod trivial-gray-streams:stream-write-byte ((stream pipe-end-for-write) byte)
   (vector-push-extend byte (get-buffer stream)))
 
 (defmethod trivial-gray-streams:stream-listen ((stream pipe-end-for-read))
-  "If the index is not on end of stream, it can probably be read."
-  ;;  This does not work with clisp, see
-  ;;  https://clisp.sourceforge.io/impnotes/non-block-io.html
+  "If the index is not on end of stream, it can probably be read.
+
+Note that this does not work on clisp, see
+https://clisp.sourceforge.io/impnotes/non-block-io.html"
   (< (get-index stream) (length (get-buffer stream))))
 
 (defun make-dummy-connection (stream &key (class 'logging-connection)
