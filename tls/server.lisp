8f8c843d48bd7d886e4cdaf6bae64366df1eb00e
Tomáš Zellerin <tomas@zellerin.cz>
2024-12-06 20:38:30 +0100

    Do not finalize TLS context, it caused crashes.

--- tls/server.lisp
+++ tls/server.lisp
@@ -53,16 +53,8 @@ We should also limit allowed ciphers, but we do not."
     (let ((topdir (asdf:component-pathname (asdf:find-system "http2"))))
       (ssl-ctx-use-certificate-chain-file context (namestring (merge-pathnames "certs/server.crt" topdir)))
       (ssl-ctx-use-private-key-file context (namestring (merge-pathnames "certs/server.key" topdir)) cl+ssl::+ssl-filetype-pem+))
-    (cl+ssl::ssl-ctx-set-alpn-select-cb
-     context
-     (cffi:get-callback 'cl+ssl::select-h2-callback))
-    (let ((address (cffi:pointer-address context)))
-      ;; make sure it is deallocated when no longer needed
-      ;; 20240827 TODO: What happens on save-and-die?
-      (trivial-garbage:finalize context
-                                (lambda () (cl+ssl:ssl-ctx-free (cffi:make-pointer address)))))))
-
-(defvar *h2-tls-context*)
+    (cl+ssl::ssl-ctx-set-alpn-select-cb context (cffi:get-callback 'cl+ssl::select-h2-callback))
+    context))
 
 (defun create-https-server (port key cert &key
                                             (connection-class 'vanilla-server-connection)
@@ -72,92 +64,77 @@ We should also limit allowed ciphers, but we do not."
 It accepts new connections and uses WRAP-TO-TLS-AND-PROCESS-SERVER-STREAM to
 establish TLS.
 
-ANNOUNCE-OPEN-FN is called, when set, to inform caller that the server is up and
-running. This is used for testing, when we need to have the server running (in a
-thread) to start testing it."
-  (create-server port 'tls-single-client-dispatcher :key key :certificate cert
-                                                    :host host
-                                                    :connection-class connection-class))
+User provided KEY and CERT-ificate.
 
+It is actually a wrapper over CREATE-SERVER with a default server
+class (TLS-SINGLE-CLIENT-DISPATCHER)."
+  (create-server port 'tls-single-client-dispatcher :certificate-file cert
+                 :private-key-file key))
 
+(defsection @server-classes
+    (:title "Server classes")
+  (tls-dispatcher-mixin class)
+  (single-client-dispatcher class)
+  (tls-single-client-dispatcher class)
+  (threaded-dispatcher class)
+  (tls-threaded-dispatcher class))
+
 ;;;; TLS dispatcher
 (defclass tls-dispatcher-mixin ()
   ((tls              :reader   get-tls              :initform :tls
                      :allocation :class)
    (certificate-file :accessor get-certificate-file :initarg  :certificate-file)
    (private-key-file :accessor get-private-key-file :initarg  :private-key-file))
-  (:documentation "Adds TLS layer to the created sockets."))
+  (:documentation
+   "Specializes SERVER-SOCKET-STREAM to add TLS layer to the created sockets,
+and START-SERVER-ON-SOCKET to use a context created by MAKE-HTTP2-TLS-CONTEXT."))
 
 (defmethod server-socket-stream (socket (dispatcher tls-dispatcher-mixin))
   "The cl-ssl server socket."
   (with-slots (certificate-file private-key-file) dispatcher
-    (cl+ssl:with-global-context (*h2-tls-context* :auto-free-p nil)
-          (cl+ssl:make-ssl-server-stream
+    (cl+ssl:make-ssl-server-stream
      (call-next-method)
      :certificate certificate-file
-     :key private-key-file))))
+     :key private-key-file))
 
-(defmethod start-server-on-socket ((dispatcher tls-dispatcher-mixin) socket)
-  "For a TLS server wrap the global context."
-  (let ((*h2-tls-context* (make-http2-tls-context)))
-    (unwind-protect (call-next-method)
-      (when *h2-tls-context*
-        (cl+ssl:ssl-ctx-free *h2-tls-context*)
-        (setf *h2-tls-context* nil)))))
+  #+nil(with-slots (certificate-file private-key-file) dispatcher
+    (cl+ssl:with-global-context (*h2-tls-context* :auto-free-p nil)
+      (cl+ssl:make-ssl-server-stream
+       (call-next-method)
+       :certificate certificate-file
+       :key private-key-file))))
 
-(defclass tls-single-client-dispatcher (tls-dispatcher-mixin detached-server-mixin single-client-dispatcher)
+"For a TLS server wrap the global context."
+(defmethod start-server-on-socket ((server tls-dispatcher-mixin) socket)
+  (cl+ssl:with-global-context ((make-http2-tls-context) :auto-free-p t)
+    (call-next-method)))
+
+(defclass tls-single-client-dispatcher (tls-dispatcher-mixin single-client-dispatcher)
+  ())
+
+(defclass tls-detached-single-client-dispatcher (tls-dispatcher-mixin detached-server-mixin single-client-dispatcher)
   ())
 
 (defclass threaded-dispatcher (base-dispatcher)
+  ()
+  (:documentation
+   "Specialize DO-NEW-CONNECTION to process new connections each in a separate thread. "))
+
+(defclass tls-threaded-dispatcher (threaded-dispatcher tls-dispatcher-mixin)
   ())
 
-(defclass tls-threaded-dispatcher (threaded-dispatcher tls-dispatcher-mixin detached-server-mixin)
+(defclass tls-threaded-dispatcher (threaded-dispatcher tls-dispatcher-mixin)
   ())
 
 (defmethod do-new-connection (listening-socket (dispatcher threaded-dispatcher))
   (let ((socket (usocket:socket-accept listening-socket
                                        :element-type '(unsigned-byte 8))))
-    (bt:make-thread
-     (lambda ()
-       (with-open-stream (stream (server-socket-stream socket dispatcher))
-         (process-server-stream stream
-                                :connection-class (get-connection-class dispatcher))))
-     ;; TODO: peer IP and port to name?
-     :name "HTTP2 server thread for connection" )))
-
-#+obsolete
-(defun create-http-server (port &key
-                                  (announce-open-fn (constantly nil))
-                                  (connection-class 'vanilla-server-connection))
-  "Open HTTP/2 server on PORT on localhost (no TLS).
-
-Callbacks defined as methods for the CONNECTION-CLASS are used to implement
-behaviour of the server.
-
-ANNOUNCE-OPEN-FN is called, when set, to inform caller that the server is up and
-running. This is used for testing, when we need to have the server running (in a
-thread) to start testing it."
-  (usocket:with-server-socket (socket (usocket:socket-listen "127.0.0.1" port
-                                                             :reuse-address t
-                                                             :backlog 200
-                                                             :element-type '(unsigned-byte 8)))
-    (funcall announce-open-fn socket)
-    (loop
-      (funcall *dispatch-fn*
-               #'process-server-stream
-               #+nil(lambda (tls-stream &rest args)
-                      (handler-case
-                          (unwind-protect
-                               (apply #'process-server-stream)
-                            (close tls-stream))
-                        (cl+ssl::ssl-error-ssl ()
-                          ;; Just ignore it for now, Maybe it should be
-                          ;; logged if not trivial - but what is trivial and
-                          ;; what is to log?
-                          )))
-               (usocket:socket-stream
-                (handler-case
-                    (usocket:socket-accept socket :element-type '(unsigned-byte 8))
-                  ;; ignore condition
-                  (usocket:connection-aborted-error ())))
-               :connection-class connection-class))))
+    (let ((stream (server-socket-stream socket dispatcher)))
+      (bt:make-thread
+       (lambda ()
+         (unwind-protect
+              (process-server-stream stream
+                                     :connection-class (get-connection-class dispatcher))
+           (close stream)))
+       ;; TODO: peer IP and port to name?
+       :name "HTTP2 server thread for connection" ))))
