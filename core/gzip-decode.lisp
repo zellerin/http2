d95d9d142909d8dc89bd24378692f2c9b1de9964
Tomáš Zellerin <tomas@zellerin.cz>
2025-01-05 19:14:44 +0100

    Make zip-buffer-size configurable.

--- core/gzip-decode.lisp
+++ core/gzip-decode.lisp
@@ -7,6 +7,7 @@
 (defmethod process-end-headers :after (connection (stream gzip-decoding-mixin))
   (setf (get-dstate stream) (chipz:make-dstate 'chipz:gzip)))
 
+(defvar *zip-buffer-size* 4096)
 (defmethod apply-data-frame :around ((stream gzip-decoding-mixin) payload start end)
   "When there is a `Content-Encoding: gzip` header, decompress the data and call next
 APPLY-DATA-FRAME methods."
@@ -17,7 +18,7 @@ APPLY-DATA-FRAME methods."
   ;; 20240607 TODO: test on dstate, not on header
 
   (if (equal (cdr (assoc "content-encoding" (get-headers stream) :test 'equal)) "gzip")
-      (let* ((buffer (make-array 4096 :element-type '(unsigned-byte 8))))
+      (let* ((buffer (make-array *zip-buffer-size* :element-type '(unsigned-byte 8))))
         (loop
           (multiple-value-bind (consumed produced)
               (chipz:decompress buffer (get-dstate stream) payload :input-start start
