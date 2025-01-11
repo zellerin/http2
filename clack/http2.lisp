6be9e7d640f5d1e8b2105c94e8aa2abc39fc7467
Tomáš Zellerin <tomas@zellerin.cz>
2025-01-11 21:54:20 +0100

    Clack fixed.

--- clack/http2.lisp
+++ clack/http2.lisp
@@ -1,14 +1,39 @@
-(cl:defpackage clack.handler.http2
-  (:use :cl :http2)
-  (:export :run))
+(mgl-pax:define-package #:clack.handler.http2
+  (:use :cl :http2/server #:http2 #:mgl-pax)
+  (:export :run)
+  (:shadow :run))
 
 (in-package clack.handler.http2)
 
-(defclass clack-server-connection (http2::server-http2-connection)
-  ((app          :accessor get-app          :initarg :app)
-   (peer-address :accessor get-peer-address :initarg :peer-address)
-   (peer-port    :accessor get-peer-port    :initarg :peer-port))
-  (:default-initargs :stream-class 'clack-server-stream))
+(defsection @clack
+    (:title "Implementing Clack interface"
+     :export nil)
+  "Clack is a web application environment that defines \"applications\" and allows
+the to be run under various CL web servers.
+
+Let us see how to make HTTP2 server usable under Clack, at least to some extent.
+
+The full code is in clack/http2.lisp file."
+  "You can run http2/server under clack by
+
+```
+    (clack:clackup
+       (lambda (env)
+         (declare (ignore env))
+         '(200 (:content-type \"text/plain\") (\"Hello, Clack! Here we go!\")))
+         :port 5052
+         :server :http2)
+```"
+  (run function)
+  (clack-server-connection class)
+  (clack-server-stream class))
+
+(defclass clack-server-connection (http2/server::vanilla-server-connection)
+  ((app            :accessor get-app            :initarg :app)
+   (peer-address   :accessor get-peer-address   :initarg :peer-address)
+   (peer-port      :accessor get-peer-port      :initarg :peer-port))
+  (:documentation "Extend vanilla-server-connection to store the application to use, and to keep
+track of the client address and port."))
 
 (defmethod initialize-instance :after ((connection clack-server-connection)
                                        &key &allow-other-keys)
@@ -16,29 +41,30 @@
     (setf peer-port nil
           peer-address nil)))
 
-(defclass clack-server-stream (http2::http2-stream-with-input-stream http2::server-stream)
+(defclass clack-server-stream (http2/stream-overlay:http2-stream-with-input-stream http2/core:server-stream)
   ((request-headers :accessor get-request-headers
                     :initarg :request-headers))
-  (:default-initargs :request-headers (make-hash-table :test 'equal)))
+  (:default-initargs :request-headers (make-hash-table :test 'equal))
+  (:documentation "Stream class that collect headers to the hash table (as Clack expects)"))
 
-(defmethod http2::add-header (connection (stream clack-server-stream) name value)
+(defmethod http2/core:add-header (connection (stream clack-server-stream) name value)
   (if (keywordp name)
       (call-next-method)
       (with-slots (request-headers) stream
         (setf (gethash name request-headers) value))))
 
-(defmethod http2::peer-ends-http-stream ((stream clack-server-stream))
+(defmethod http2/core:peer-ends-http-stream ((stream clack-server-stream))
   (let ((response
-          (funcall (get-app (http2::get-connection stream))
+          (funcall (get-app (http2/core::get-connection stream))
                    (let* ((parsed-uri (puri:parse-uri (http2::get-path stream)))
-                          (server-name-and-port (http2::get-authority stream))
+                          (server-name-and-port (http2/core::get-authority stream))
                           (colon-pos (position #\: server-name-and-port)))
                      (list
-                      :request-method (http2::get-method stream)
+                      :request-method (http2/core::get-method stream)
                       :script-name ""
                       :path-info (puri:uri-path parsed-uri)
                       :query-string (puri:uri-query parsed-uri)
-                      :url-scheme (http2::get-scheme stream)
+                      :url-scheme (http2/core::get-scheme stream)
                       :server-name (subseq server-name-and-port 0 colon-pos)
                       :server-port (if colon-pos
                                        (parse-integer
@@ -48,9 +74,9 @@
                       :request-uri (http2::get-path stream)
                       :raw-body stream
                       :remote-addr (get-peer-address
-                                    (http2::get-connection stream))
+                                    (http2/core::get-connection stream))
                       :remote-port (get-peer-port
-                                    (http2::get-connection stream))
+                                    (http2/core::get-connection stream))
                       :headers (get-request-headers stream))))))
     (if (consp response)
         (destructuring-bind (status headers body) response
@@ -58,7 +84,7 @@
                                      (loop for (key value) on headers by 'cddr
                                            collect (list (string-downcase (symbol-name key))
                                                          (princ-to-string value)))))
-          (with-open-stream (out (make-transport-output-stream stream :utf8 nil))
+          (with-open-stream (out (http2/core:make-transport-output-stream stream :utf8 nil))
             (etypecase body
               (cons ; list of streams
                (dolist (string body)
@@ -66,7 +92,7 @@
               (vector
                (write-sequence body out))
               (pathname
-               (let ((buffer (make-octet-buffer 4096)))
+               (let ((buffer (http2/utils:make-octet-buffer 4096)))
                  (with-open-file (in body :element-type '(unsigned-byte 8))
                    (loop for len = (read-sequence buffer in)
                          while (plusp len)
@@ -84,7 +110,7 @@
                                          collect (list (string-downcase (symbol-name key))
                                                        value)))))
     (lambda (body &key (start 0) (end (length body)) &allow-other-keys)
-      (with-open-stream (out (make-transport-output-stream stream :utf8 nil))
+      (with-open-stream (out (http2/core:make-transport-output-stream stream :utf8 nil))
         (etypecase body
           (string
            (write-sequence
@@ -102,7 +128,12 @@
 (defun run (app &key port (ssl-key-file (first *default-certificate-pair*))
                     (ssl-cert-file (second *default-certificate-pair*))
                     fd)
+  "Implement HTTP/2 server. Called by Clask when started with :server http2."
   (when fd (error "cannot listen on FD"))
-  (http2:create-https-server port ssl-key-file ssl-cert-file
-                             :connection (make-instance 'clack-server-connection
-                                                        :app app)))
+  (http2/server:start port
+                      :dispatcher (make-instance http2/server::*vanilla-server-dispatcher*
+                                                 :private-key-file  ssl-key-file
+                                                 :certificate-file ssl-cert-file
+                                                 :connection-class 'clack-server-connection
+                                                 :connection-args `(:app ,app
+                                                                    :stream-class clack-server-stream))))
