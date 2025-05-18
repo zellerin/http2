a69fd4092dd60867f349865da1782ed9268e9fc3
Tomáš Zellerin <tomas@zellerin.cz>
2025-05-18 17:33:01 +0200

    Add CFFI interfaces for connect &c

--- server/poll-grovel.lisp
+++ server/poll-grovel.lisp
@@ -27,3 +27,12 @@
 (constant (c-pollnval "POLLNVAL"))
 
 (constant (eagain "EAGAIN"))
+(constant (einprogress "EINPROGRESS"))
+
+(constant (pf-inet "PF_INET"))
+(constant (af-inet "AF_INET"))
+(constant (sock-stream "SOCK_STREAM"))
+(cstruct sockaddr-in "struct sockaddr_in"
+         (sin-family "sin_family" :type :int16)
+         (sin-port "sin_port" :type :uint16)
+         (sin-addr "sin_addr" :type :uint32))
