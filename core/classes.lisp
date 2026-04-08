032a21932d73f92d31529f8c13898ec5f7f6eb0a
Tomáš Zellerin <tomas@zellerin.cz>
2026-04-08 18:10:06 +0200

    Use QUEUE-FRAME to send the client preface. (check: undef fn?)
    It means that the client can be used in the poll server, where there is no
    network stream.
    
    There might or might not be a problem with QUEUE-FRAME defined only later.

--- core/classes.lisp
+++ core/classes.lisp
@@ -405,8 +405,7 @@ Then write the initial settings frame, and expect normal frame. Actually, this s
    The client connection preface starts with a sequence of 24 octets.  This
    sequence MUST be followed by a SETTINGS frame (Section 6.5), which MAY be
    empty."
-  ;; 20240708 TODO: This should go to client, or at least not depend on network stream
-  (write-sequence +client-preface-start+ (get-network-stream connection))
+  (queue-frame connection +client-preface-start+)
   (write-settings-frame connection (get-settings connection)))
 
 
