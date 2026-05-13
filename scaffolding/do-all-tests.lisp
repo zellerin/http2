5d3f41271b4f4d9e49d7b31a7c5b4da635a8529a
Tomáš Zellerin <tomas@zellerin.cz>
2026-05-13 17:09:53 +0200

    Use timeouts, do not log server outputs in scaffolding/do-all-tests

--- scaffolding/do-all-tests.lisp
+++ scaffolding/do-all-tests.lisp
@@ -9,4 +9,7 @@
 (asdf:load-asd (truename "./http2.asd"))
 (asdf:load-system "mgl-pax")
 (asdf:load-system "http2")
+(setf http2/server:*logged-events* http2/server:+log-nothing+
+      http2/server::*no-client-poll-timeout* 1
+      http2/server::*poll-timeout* 1)
 (asdf:test-system "http2")
